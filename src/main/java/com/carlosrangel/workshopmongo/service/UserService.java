fe981c7658030886f29e3f0d00b5bbfc636f03d1
Carlos Rangel <croi7726@gmail.com>
2025-02-17 14:31:35 -0300

    Obtendo um usuario por Id

--- src/main/java/com/carlosrangel/workshopmongo/service/UserService.java
+++ src/main/java/com/carlosrangel/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.carlosrangel.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carlosrangel.workshopmongo.domain.User;
 import com.carlosrangel.workshopmongo.repository.UserRepository;
+import com.carlosrangel.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,6 +20,9 @@ public class UserService {
 		return repo.findAll();
 	}
 	
-	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Não"));
+	}
 	
 }
